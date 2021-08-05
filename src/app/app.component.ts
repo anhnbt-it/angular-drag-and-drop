d661b06f05e60f064616d4fdb3db51ddd2c4dcec
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-08-05 09:16:00 +0700

    [feat] get data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {HttpClient} from '@angular/common/http';
+
+interface ICard {
+  id?: number;
+  title: string;
+}
+
+interface IList {
+  id?: number;
+  title: string;
+  board_cards: ICard[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'draganddrop';
+  lists: IList[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  loadDataFromServer(): void {
+    this.http.get<IList[]>('http://127.0.0.1:8000/api/lists').subscribe((resp: IList[]) => {
+      this.lists = resp;
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadDataFromServer();
+  }
+
+  dropList(event: CdkDragDrop<IList[]>): void {
+    console.log('dropList', event);
+    moveItemInArray(this.lists, event.previousIndex, event.currentIndex);
+  }
+
+  dropCard(event: CdkDragDrop<ICard[]>): void {
+    console.log('dropCard', event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {ListComponent} from './list/list.component';
+import {CardComponent} from './card/card.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
